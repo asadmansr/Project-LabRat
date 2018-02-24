b26ee080c1d7ac63d4cdce6dd0f43f162085c70a
asadmansr <asadmansr@gmail.com>
2018-02-24 02:14:56 -0500

    Implemented Login Screen on LabRat-Android

---
+++ LabRat-Android/app/src/main/java/com/asadmansoor/labrat_android/LoginActivity.kt
@@ -0,0 +1,78 @@
+package com.asadmansoor.labrat_android
+
+
+import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class LoginActivity : AppCompatActivity(){
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        password.setOnEditorActionListener(TextView.OnEditorActionListener { _, id, _ ->
+            if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                attemptLogin()
+                return@OnEditorActionListener true
+            }
+            false
+        })
+
+        email_sign_in_button.setOnClickListener { attemptLogin() }
+    }
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private fun attemptLogin() {
+
+        // Reset errors.
+        email.error = null
+        password.error = null
+
+        // Store values at the time of the login attempt.
+        val emailStr = email.text.toString()
+        val passwordStr = password.text.toString()
+
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(passwordStr) && !isPasswordValid(passwordStr)) {
+            password.error = getString(R.string.error_invalid_password)
+        } else if (TextUtils.isEmpty(emailStr)) {
+            email.error = getString(R.string.error_field_required)
+
+        } else if (!isEmailValid(emailStr)) {
+            email.error = getString(R.string.error_invalid_email)
+        } else {
+            executeLogin()
+        }
+
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        //TODO: Replace this with your own logic
+        return email.contains("@")
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        //TODO: Replace this with your own logic
+        return password.length > 4
+    }
+
+    private fun executeLogin(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
---
+++ LabRat-Android/app/src/main/java/com/asadmansoor/labrat_android/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.asadmansoor.labrat_android
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed({loginScreen()},3000)
+    }
+
+    private fun loginScreen(){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
